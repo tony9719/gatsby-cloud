7c42aee570e05817a1b67f28ac1811ed38502790
Tony de Vita <tony.devita97@gmail.com>
2023-07-27 00:20:01 +0200

    Fineshed Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -10,6 +10,12 @@ const BlogPost = ({data, children}) => {
         <Layout pageTitle={data.mdx.frontmatter.title}>
             <p>Posted: {data.mdx.frontmatter.date}</p>
             <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt}/>
+            <p>
+                Photo Credit:{" "}
+                <a href={data.mdx.frontmatter.hero_image_credit_link}>
+                    {data.mdx.frontmatter.hero_image_credit_text}
+                </a>
+            </p>
             {children}
         </Layout>
     )
